4098ac4d2c5c26b33a53775e39f823f614731de7
Gautam Chakrabarty <2gautam.c@gmail.com>
2023-04-11 19:04:59 +0100

    Making progress in translate_sequence function

--- translate.py
+++ translate.py
@@ -28,7 +28,26 @@ def translate_sequence(rna_sequence, genetic_code):
     str
         A string of the translated amino acids.
     """
-    pass
+    
+    if len(rna_sequence) < 3:
+        return ''
+
+    i = 0
+    amino = ''
+    rna_sequence = rna_sequence.upper()
+
+    while i < len(rna_sequence):
+        rna = rna_sequence[i:i+3]
+        if rna in genetic_code:
+            if genetic_code[rna] == "*":
+                if i == 0:
+                    return ''
+                break
+            amino += genetic_code[rna]
+            i += 3
+
+    return amino
+
 
 def get_all_translations(rna_sequence, genetic_code):
     """Get a list of all amino acid sequences encoded by an RNA sequence.
@@ -149,29 +168,32 @@ def get_longest_peptide(rna_sequence, genetic_code):
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
-    rna_seq = ("AUG"
-            "UAC"
-            "UGG"
-            "CAC"
-            "GCU"
-            "ACU"
-            "GCU"
-            "CCA"
-            "UAU"
-            "ACU"
-            "CAC"
-            "CAG"
-            "AAU"
-            "AUC"
-            "AGU"
-            "ACA"
-            "GCG")
-    longest_peptide = get_longest_peptide(rna_sequence = rna_seq,
-            genetic_code = genetic_code)
-    assert isinstance(longest_peptide, str), "Oops: the longest peptide is {0}, not a string".format(longest_peptide)
-    message = "The longest peptide encoded by\n\t'{0}'\nis\n\t'{1}'\n".format(
-            rna_seq,
-            longest_peptide)
-    sys.stdout.write(message)
-    if longest_peptide == "MYWHATAPYTHQNISTA":
-        sys.stdout.write("Indeed.\n")
+    # rna_seq = ("AUG"
+    #         "UAC"
+    #         "UGG"
+    #         "CAC"
+    #         "GCU"
+    #         "ACU"
+    #         "GCU"
+    #         "CCA"
+    #         "UAU"
+    #         "ACU"
+    #         "CAC"
+    #         "CAG"
+    #         "AAU"
+    #         "AUC"
+    #         "AGU"
+    #         "ACA"
+    #         "GCG")
+    # longest_peptide = get_longest_peptide(rna_sequence = rna_seq,
+    #         genetic_code = genetic_code)
+    # assert isinstance(longest_peptide, str), "Oops: the longest peptide is {0}, not a string".format(longest_peptide)
+    # message = "The longest peptide encoded by\n\t'{0}'\nis\n\t'{1}'\n".format(
+    #         rna_seq,
+    #         longest_peptide)
+    # sys.stdout.write(message)
+    # if longest_peptide == "MYWHATAPYTHQNISTA":
+    #     sys.stdout.write("Indeed.\n")
+
+    rna_seq = ('GUCGAACGAA')
+    print(translate_sequence(rna_seq, genetic_code))
